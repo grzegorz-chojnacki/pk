26730522bb244a4749b553ca8464e26fb0185c1e
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-05-02 13:42:58 +0200

    Signing & verifying

--- lab05/elgamal.py
+++ lab05/elgamal.py
@@ -6,6 +6,7 @@
 
 import getopt
 import sys
+import math as m
 import random as r
 
 
@@ -29,10 +30,10 @@ def generate_keys():
 
 def encrypt():
     with open('public.txt') as public, open('plain.txt') as plain:
+        msg = int(plain.readline())
         p = int(public.readline())
         g = int(public.readline())
         public_key = int(public.readline())
-        msg = int(plain.readline())
 
         assert msg < p
         encryption_key = r.randint(1, p - 1)
@@ -44,11 +45,11 @@ def encrypt():
 
 def decrypt():
     with open('private.txt') as private, open('crypto.txt') as crypto:
-        p = int(private.readline())
-        g = int(private.readline())
-        private_key = int(private.readline())
         crp_key = int(crypto.readline())
         crp_msg = int(crypto.readline())
+        p = int(private.readline())
+        _ = int(private.readline())
+        private_key = int(private.readline())
 
         shadow = pow(crp_key, private_key, p)
         e = pow(shadow, -1, p)
@@ -58,19 +59,50 @@ def decrypt():
         decrypt.write(f'{msg}\n')
 
 
+def random_coprime(p):
+    while True:
+        sign_key = r.randint(2, p - 1)
+        if m.gcd(sign_key, p) == 1:
+            yield sign_key
+
+
 def sign():
     with open('private.txt') as private, open('message.txt') as message:
-        pass
+        msg = int(message.readline())
+        p = int(private.readline())
+        g = int(private.readline())
+        private_key = int(private.readline())
+
+        assert msg < p
+
+        sign_key = next(random_coprime(p - 1))
+        sign_key_e = pow(sign_key, -1, p - 1)
+        r = pow(g, sign_key, p)
+        x = (msg - private_key * r) * sign_key_e % (p - 1)
+
     with open('signature.txt', 'w') as signature:
-        pass
+        signature.write(f'{r}\n')
+        signature.write(f'{x}\n')
 
 
 def verify():
-    with open('public.txt') as public, open('message.txt') as message:
-        pass
+    with open('public.txt') as public, open('message.txt') as message, open('signature.txt') as signature:
+        msg = int(message.readline())
+        r = int(signature.readline())
+        x = int(signature.readline())
+        p = int(public.readline())
+        g = int(public.readline())
+        public_key = int(public.readline())
+
+        left = pow(g, msg, p)
+        right = pow(r, x, p) * pow(public_key, r, p) % p
+        print(left)
+        print(right)
+
     with open('verify.txt', 'w') as verify:
-        pass
-    return True
+        result = 'T' if left == right else 'N'
+        verify.write(f'{result}\n')
+    return result
 
 
 def main():
@@ -93,8 +125,7 @@ def main():
             sign()
             print('Wyprodukowano podpis')
         elif opt == '-v':
-            v = verify()
-            print('T' if v else 'N')
+            print(verify())
         else:
             raise getopt.GetoptError('')
 
@@ -109,7 +140,7 @@ def main():
         sys.exit(3)
     except AssertionError:
         print(f'Szyfrowana wiadomość nie spełnia warunku: m < p')
-        sys.exit(3)
+        sys.exit(4)
 
 
 if __name__ == "__main__":
