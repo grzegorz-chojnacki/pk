404f4fa2f2851523c445200d18e51a1ff2f058e2
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-05-22 21:47:40 +0200

    Added first algorithm

--- lab06/stegano.py
+++ lab06/stegano.py
@@ -6,24 +6,7 @@
 
 import getopt
 import sys
-from math import gcd
-from random import randint
-
-
-def line_end_space(msg):
-    pass
-
-
-def single_double_space(msg):
-    pass
-
-
-def fake_typo_insertion(msg):
-    pass
-
-
-def useless_tag_insertion(msg):
-    pass
+import itertools as it
 
 
 def encrypt(msg):
@@ -39,6 +22,7 @@ def chunks(array, n):
     for i in range(0, len(array), n):
         yield array[i:i + n]
 
+
 def decrypt(bits):
     def transform(bits):
         binary = int(''.join(bits), 2)
@@ -47,32 +31,73 @@ def decrypt(bits):
     return ''.join(map(transform, chunks(bits, 4)))
 
 
+def line_end_space_encrypt(bits):
+    with open('cover.html') as src, open('watermark.html', 'w') as dst:
+        lines = [line[:-1] + ' \n' if bit == '1' else line
+                 for (bit, line) in it.zip_longest(bits, src.readlines())]
+        dst.writelines(lines)
+
+
+def line_end_space_decrypt():
+    with open('watermark.html') as src, open('detect.txt', 'w') as dst:
+        bits = ['1' if len(line) >= 2 and line[-2] == ' ' else '0'
+                for line in src.readlines()]
+
+        dst.writelines(decrypt(bits))
+
+
+def single_double_space_encrypt(bits):
+    pass
+
+
+def single_double_space_decrypt():
+    pass
+
+
+def fake_typo_insertion_encrypt(bits):
+    pass
+
+
+def fake_typo_insertion_decrypt():
+    pass
+
+
+def useless_tag_insertion_encrypt(bits):
+    pass
+
+
+def useless_tag_insertion_decrypt():
+    pass
+
+
 def main():
     try:
-        opts, _ = getopt.getopt(sys.argv[1:], 'de1234')
-        operation = method = None
+        # opts, _ = getopt.getopt(sys.argv[1:], 'de1234')
+        # operation = encryption = None
 
-        for o, _ in opts:
-            if o == '-e':
-                method = encrypt
-            elif o == '-d':
-                method = decrypt
-            elif o == '-1':
-                operation = line_end_space
-            elif o == '-2':
-                operation = single_double_space
-            elif o == '-3':
-                operation = fake_typo_insertion
-            elif o == '-4':
-                operation = useless_tag_insertion
+        # for o, _ in opts:
+        #     if o == '-e':
+        #         encryption = True
+        #     elif o == '-d':
+        #         encryption = False
 
-        if operation is None or method is None:
-            raise getopt.GetoptError('')
-        else:
-            with open('mess.txt') as mess:
-                msg = mess.read()
-                print(decrypt(encrypt(msg)))
+        # for o, _ in opts:
+        #     if o == '-1':
+        #         operation = line_end_space_encrypt if encryption else line_end_space_decrypt
+        #     elif o == '-2':
+        #         operation = single_double_space_encrypt if encryption else single_double_space_decrypt
+        #     elif o == '-3':
+        #         operation = fake_typo_insertion_encrypt if encryption else fake_typo_insertion_decrypt
+        #     elif o == '-4':
+        #         operation = useless_tag_insertion_encrypt if encryption else useless_tag_insertion_decrypt
 
+        # if operation is None or encryption is None:
+        #     raise getopt.GetoptError('')
+
+        with open('mess.txt') as mess:
+            msg = mess.read()
+            line_end_space_encrypt(encrypt(msg))
+            line_end_space_decrypt()
 
     except getopt.GetoptError:
         print(f'użycie: {sys.argv[0]} -[de] -[1234]')
